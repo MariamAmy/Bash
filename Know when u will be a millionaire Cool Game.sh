311cd45882769d3603a27230934264bed6d94db0
Mariam <mariam.ayman@ejust.edu.eg>
2023-08-03 18:46:15 +0300

    Updated

---
+++ Know when u will be a millionaire Cool Game.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Know when u will be a Millionaire? Cool Game! 
+
+user=$(whoami)
+where=$(pwd)
+date=$(date)
+
+echo "Let's know when you will be a millionaire! Cool Game!"
+
+echo "What is you name ?"
+read name
+
+echo "What is your age ?"
+read age
+
+echo "Welcome $namne, you are $age years old"
+
+echo  "Calculating when you will be a millionaire..."
+
+sleep 2
+
+x=$((( $RANDOM % 10 ) + $age ))
+
+echo "$name, u will be a millionaire when u become $x !!" 
+
